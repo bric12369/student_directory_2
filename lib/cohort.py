aa4c238021c7448ed9c5ccee94046b5cf21be491
Barry <barryrichards05@gmail.com>
2026-02-06 11:02:52 +0000

    added equality and formatting

--- lib/cohort.py
+++ lib/cohort.py
@@ -1,4 +1,11 @@
 class Cohort:
-    def __init__(self, name, start_date):
+    def __init__(self, id, name, start_date):
+        self.id = id
         self.name = name
         self.start_date = start_date
+
+    def __eq__(self, value):
+        return self.__dict__ == value.__dict__
+    
+    def __repr__(self):
+        return f'Cohort({self.id}, {self.name}, {self.start_date})'
--- tests/test_cohort.py
+++ tests/test_cohort.py
@@ -1,6 +1,16 @@
 from lib.cohort import *
 
 def test_init_with_attributes():
-    cohort = Cohort('Blue', '12/12/2025')
+    cohort = Cohort(1, 'Blue', '12/12/2025')
+    assert cohort.id == 1
     assert cohort.name == 'Blue'
     assert cohort.start_date == '12/12/2025'
+
+def test_instances_with_same_attributes_equal():
+    cohort1 = Cohort(1, 'Blue', '12/12/2025')
+    cohort2 = Cohort(1, 'Blue', '12/12/2025')
+    assert cohort1 == cohort2
+
+def test_format():
+    cohort = Cohort(1, 'Blue', '12/12/2025')
+    assert str(cohort) == 'Cohort(1, Blue, 12/12/2025)'
